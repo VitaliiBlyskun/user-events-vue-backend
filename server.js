7ae3cd8074eb54d4d19f91c0ff5b28dd5a5b4900
Vitalii Blyskun <97634483+VitaliiBlyskun@users.noreply.github.com>
2023-04-24 23:23:10 +0300

    add process.env

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 
-const{PORT = 3000} = process.env;
-
-const DB_HOST = "mongodb+srv://Vitalii:Mango970512prettyboy@cluster0.dueofuh.mongodb.net/db_users_events?retryWrites=true&w=majority"
+const { DB_HOST, PORT } = process.env;
 
 mongoose
   .connect(DB_HOST)
@@ -15,5 +13,3 @@ mongoose
     console.log(error.message);
     process.exit(1);
   });
-
-  // доробляти
