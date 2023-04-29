fe290f0b51dafe1ede8cf1798a5d9746e502a7df
Vitalii Blyskun <97634483+VitaliiBlyskun@users.noreply.github.com>
2023-04-29 18:23:56 +0300

    add changes

--- controllers/events/fetchEvent.js
+++ controllers/events/fetchEvent.js
@@ -1,34 +1,9 @@
 const { Event } = require("../../models");
-const { HttpError } = require("../../utils");
-
-// const fetchEvent = async (req, res) => {
-//         const id = req.params.id;
-//         try {
-//             const events = await Event.find({ user_id: id });
-//             res.status(200).json(events);
-//         } catch (err) {
-//             res.status(404).json({ message: err.message });
-//         }
-// }
-
-// const fetchEvent = async (request, response) => {
-//   const { id } = request.params;
-//   const event = await Event.find({ event_id: id });
-//   console.log("result:", event);
-//   if (!event) {
-//     throw HttpError(404, "User not found");
-//   }
-//   response.json({
-//     status: "success",
-//     code: 200,
-//     data: {
-//       result: event,
-//     },
-//   });
-// };
 
 const fetchEvent = async (request, response) => {
-  const events = await Event.find();
+  const { page = 1, limit = 10 } = request.query
+  const skip = (page -1) * limit
+  const events = await Event.find({}, "-createdAt -updatedAt", {skip, limit});
   response.json({
     status: "success",
     code: 200,
--- controllers/users/getAll.js
+++ controllers/users/getAll.js
@@ -1,7 +1,9 @@
 const { User } = require("../../models");
 
 const getAll = async (request, response) => {
-  const users = await User.find();
+  const { page = 1, limit = 10 } = request.query
+  const skip = (page -1) * limit
+  const users = await User.find({}, "-createdAt -updatedAt", {skip, limit});
   response.json({
     status: "success",
     code: 200,
