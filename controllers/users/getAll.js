c93be1c5fcbc325fa3ef530b92a772d32e5a8d3c
Vitalii Blyskun <97634483+VitaliiBlyskun@users.noreply.github.com>
2023-05-02 00:03:01 +0300

    add changes

--- controllers/users/getAll.js
+++ controllers/users/getAll.js
@@ -1,14 +1,44 @@
+// const { User } = require("../../models");
+
+// const getAll = async (request, response) => {
+//   const { page = 1, limit = 50 } = request.query
+//   const skip = (page -1) * limit
+//   const users = await User.find({}, "-createdAt -updatedAt", {skip, limit});
+//   response.json({
+//     status: "success",
+//     code: 200,
+//     data: {
+//       result: users,
+//     },
+//   });
+// };
+
+// module.exports = getAll;
+
+
 const { User } = require("../../models");
 
 const getAll = async (request, response) => {
-  const { page = 1, limit = 50 } = request.query
-  const skip = (page -1) * limit
-  const users = await User.find({}, "-createdAt -updatedAt", {skip, limit});
-  response.json({
+  const { page = 1, limit = 50 } = request.query;
+  const skip = (page - 1) * limit;
+
+  const users = await User.find({}, "-createdAt -updatedAt", {skip, limit}).populate('events');
+
+  const usersWithEventsCount = users.map(user => ({
+    _id: user._id,
+    userName: user.userName,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    phone: user.phone,
+    eventsCount: user.events.length,
+  }));
+
+  response.status(200).json({
     status: "success",
     code: 200,
     data: {
-      result: users,
+      users: usersWithEventsCount,
     },
   });
 };
