097d4103ada5ad566a2bf00158a09a61b66a02b2
Vitalii Blyskun <97634483+VitaliiBlyskun@users.noreply.github.com>
2023-05-02 14:13:41 +0300

    return code

--- controllers/users/addUserEvent.js
+++ controllers/users/addUserEvent.js
@@ -1,73 +1,36 @@
-// const { User, Event } = require("../../models");
-// const { HttpError } = require("../../utils");
-
-// const addUserEvent = async (req, res) => {
-//   const { title, description, startDate, endDate } = req.body;
-//   const userId = req.params.userId;
-
-//     const user = await User.findById(userId);
-//     if (!user) {
-//       throw HttpError(404, "User not found");
-//     }
-
-//     const newEvent = new Event({
-//       title,
-//       description,
-//       startDate,
-//       endDate,
-//       userId: user._id,
-//     });
-
-//     const result = await newEvent.save();
-
-//     user.events.push(result._id);
-//     await user.save();
-
-//     res.status(201).json({
-//       status: "success",
-//       code: 201,
-//       data: {
-//         event: result,
-//       },
-//     });
-
-// };
-
-// module.exports = addUserEvent;
-
-
 const { User, Event } = require("../../models");
 const { HttpError } = require("../../utils");
 
-const addUserEvent = async (request, response) => {
-  const { title, description, startDate, endDate } = request.body;
-  const userId = request.params.userId;
+const addUserEvent = async (req, res) => {
+  const { title, description, startDate, endDate } = req.body;
+  const userId = req.params.userId;
 
-  const user = await User.findOne({ _id: userId });
+    const user = await User.findById(userId);
+    if (!user) {
+      throw HttpError(404, "User not found");
+    }
 
-  if (!user) {
-    throw HttpError(404, "User not found");
-  }
+    const newEvent = new Event({
+      title,
+      description,
+      startDate,
+      endDate,
+      userId: user._id,
+    });
 
-  const result = await Event.create({
-    title,
-    description,
-    startDate,
-    endDate,
-    userId,
-  });
+    const result = await newEvent.save();
 
-  user.events.push(result._id);
+    user.events.push(result._id);
+    await user.save();
 
-  await user.save();
+    res.status(201).json({
+      status: "success",
+      code: 201,
+      data: {
+        event: result,
+      },
+    });
 
-  response.status(201).json({
-    status: "success",
-    code: 201,
-    data: {
-      event: result,
-    },
-  });
 };
 
 module.exports = addUserEvent;
