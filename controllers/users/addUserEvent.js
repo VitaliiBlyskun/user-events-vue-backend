64b2bd5010c72882ab0fba74b040d0ccb172f5d7
Vitalii Blyskun <97634483+VitaliiBlyskun@users.noreply.github.com>
2023-05-02 19:37:58 +0300

    changes

--- controllers/users/addUserEvent.js
+++ controllers/users/addUserEvent.js
@@ -1,9 +1,13 @@
 const { User, Event } = require("../../models");
 const { HttpError } = require("../../utils");
 
-const addUserEvent = async (req, res) => {
-  const { title, description, startDate, endDate } = req.body;
-  const userId = req.params.userId;
+const addUserEvent = async (request, response) => {
+  const { title, description, startDate, endDate } = request.body;
+  const userId = request.params.userId;
+
+      if (typeof userId !== 'string') {
+    return response.status(400).json({ message: 'userId must be a string' });
+  }
 
     const user = await User.findById(userId);
     if (!user) {
@@ -23,7 +27,7 @@ const addUserEvent = async (req, res) => {
     user.events.push(result._id);
     await user.save();
 
-    res.status(201).json({
+    response.status(201).json({
       status: "success",
       code: 201,
       data: {
